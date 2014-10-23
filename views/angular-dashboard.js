04322879a104bc2c73d9c4e55e919bffed7403e9
Kevin Lau <kevin.lau31@gmail.com>
2014-10-22 20:47:49 -0500

    Tried to get personal stats, but stuck on retreiving data from HTTP GET

--- views/angular-dashboard.js
+++ views/angular-dashboard.js
@@ -1,12 +1,43 @@
 (function(){
 	var app = angular.module('dashboard', []);
 
-	app.controller('dataPlaceholder', function(){
-		//TODO: find what data we need and initialize it here
-		//this.data = some mongo query
+	app.service('stat', function($http){
+		this.getStats = function(callback){
+			var url = 'http://demo7576728.mockable.io/personal';
+			$http.get(url).success(function(response){
+				console.log('base response = ' +response);
+				callback(response);
+			})
+		}
 	});
 
-	app.controller('TabController', function($http, $q){
+	app.controller('PersonalController', ['$scope', '$http', 'stat', function($scope, $http, stat){
+		//TODO: find what data we need and initialize it here
+		//this.data = some mongo query
+		this.lastusage;
+		this.lastduration
+		this.datejoined;
+		this.numshowers;
+		this.init = function(){
+			var url = 'http://demo7576728.mockable.io/personal';
+			stat.getStats(function(response){
+				$scope.profile = response;
+				// console.log('$scope.profile = ' +Object.keys($scope.profile))
+				console.log(response); //successfully got response here, TODO: BUT HOW TO USE IT
+			});
+			// this.lastusage = $scope.profile.lastShowerUsage;
+			// this.lastduration = data.lastShowerDuration;
+			// this.datejoined = data.created;
+			// this.numshowers = data.numShowers;
+		};
+
+		this.init();
+
+	}]);
+
+
+
+	app.controller('TabController', function($http, $scope){
 		this.setTab = function(num){
 			this.tab = num;
 			var url;
@@ -15,68 +46,64 @@
 					this.period = "this week";
 					url = 'http://demo7576728.mockable.io/week1';
 					break;
-				case 2:
-					this.period = "this month";
-					url = 'http://demo7576728.mockable.io/month1';
-					break;
-			}
+					case 2:
+						this.period = "this month";
+						url = 'http://demo7576728.mockable.io/month1';
+						break;
+					}
 
-		// var usage = function($q){
-		// 	var defer = $q.defer();
-			$http.get(url).success(function(data){
-				usageChart.load({
-					json: data.values
-				});
-				// defer.resolve(data.total);
+					$scope.usage;
+					$http.get(url).success(function(data){
+						usageChart.load({
+							json: data.values
+						});
+						$scope.usage = data.total;
+					});
+					this.usagetotal = $scope.usage;
+					// $http.get(url).success(loadUsage);
+
+				};	//end of setTab function
+
+				this.isSet = function(num){
+					return (this.tab == num);
+				};
+
+				//Refresh chart with new data
+				loadUsage = function(data){
+					usageChart.load({
+						json: data.values
+					});
+					$scope.usage = data.total;
+				};
+
+				//Initialize page to first tab (Today)
+				this.setTab(1);
 			});
-		// 	return defer.promise;
-		// };
 
-		// this.usagetotal = usage;
+			app.controller('LeaderboardController', function($http){
+				this.addFriend = function(){
+					$http.get(url).success(loadData);
+				}
+				this.removeFriend = function(){
+					$http.get(url).success(loadData);
+				}
 
-
-		};	//end of setTab function
-
-		this.isSet = function(num){
-			return (this.tab == num);
-		};
-
-		//Refresh chart with new data
-		// loadData = function(data){
-		// 	usageChart.load({
-		// 		json: data.values
-		// 	});
-		// 	defer.resolve(data.total);
-		// };
-
-		//Initialize page to first tab (Today)
-		this.setTab(1);
-	});
-
-	app.controller('LeaderboardController', function($http){
-		this.addFriend = function(){
-			$http.get(url).success(loadData);
-		}
-		this.removeFriend = function(){
-			$http.get(url).success(loadData);
-		}
-
-		this.initialize = function(){
-			var url = 'http://demo7576728.mockable.io/leaderboard';
-			$http.get(url).success(loadData);
+				this.initialize = function(){
+					var url = 'http://demo7576728.mockable.io/leaderboard';
+					$http.get(url).success(loadData);
 
 
-		};
+				};
 
-		loadData = function(data){
-			leaderboard.load({
-				json: data.values
+				loadData = function(data){
+					leaderboard.load({
+						json: data.values
+					});
+				};
+
+				this.initialize();
+
 			});
-		};
-
-		this.initialize();
-
-	});
 
 
-})();
+		})();
