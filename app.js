f4186802578fdfca2401c0f89d1d9a364ba7be5b
Sourabh Shirhatti <shirhatti@outlook.com>
2014-11-14 20:52:06 -0600

    Shower API completed

--- app.js
+++ app.js
@@ -149,23 +149,95 @@ app.post('/api/wristband', function(req, res){
   
 });
 
-app.post('/api/shower', function(req, res){
-  console.log(req.query.wristband+" "+req.query.duration+" "+req.query.volume);
-  var shower = new Shower({
-    wristbandID: req.query.wristband,
-    duration: req.query.duration,
-    timeStamp: Date.now(),
-    volume: req.query.volume,
-  });
-  res.send(200);
-  shower.save(function(err) {
-    if(err) { 
+app.get('/api/shower/month', function(req, res) {
+  User.findById(req.session.passport.user, function(err, userzz) {
+    if(err) {
+      res.send("Unauthorized", 401);
       console.log(err); 
+    }
+    if (!err && userzz!= null) {
+      var today = new Date();
+      var lastMonth = new Date();
+      lastMonth.setDate(today.getDate() - 30);
+
+      Shower.aggregate([
+          { $match: { wristbandID: 4 , date: { $gte: lastMonth, $lt: today} }},
+          { $group: {
+              _id : { year: { $year: "$date" }, month: { $month: "$date" }, day: { $dayOfMonth: "$date" } },
+              total: { $sum: "$waterConsumed" },
+              duration: { $sum: "$duration" },
+              count: { $sum: 1}
+          }},
+          { $sort: { _id: -1 }}//,
+          // { $limit: 30 }
+
+        ], function (err, showers) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.send(showers);
+      });
     } else {
-      console.log('saving shower ...');
-    };
-  });
-});
+      res.send("Unauthorized", 401);
+      console.log(err); 
+    }
+  })
+})
+
+app.get('/api/shower/week', function(req, res) {
+  User.findById(req.session.passport.user, function(err, userzz) {
+    if(err) {
+      res.send("Unauthorized", 401);
+      console.log(err); 
+    }
+    if (!err && userzz!= null) {
+      var today = new Date();
+      var lastWeek = new Date();
+      lastWeek.setDate(today.getDate() - 7);
+
+      Shower.aggregate([
+          { $match: { wristbandID: 4 , date: { $gte: lastWeek, $lt: today} }},
+          { $group: {
+              _id : { year: { $year: "$date" }, month: { $month: "$date" }, day: { $dayOfMonth: "$date" } },
+              total: { $sum: "$waterConsumed" },
+              duration: { $sum: "$duration" },
+              count: { $sum: 1}
+          }},
+          { $sort: { _id: -1 }}//,
+          // { $limit: 30 }
+
+        ], function (err, showers) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.send(showers);
+      });
+    } else {
+      res.send("Unauthorized", 401);
+      console.log(err); 
+    }
+  })
+})
+
+// app.post('/api/shower', function(req, res){
+//   console.log(req.query.wristband+" "+req.query.duration+" "+req.query.volume);
+//   var shower = new Shower({
+//     wristbandID: req.query.wristband,
+//     duration: req.query.duration,
+//     timeStamp: Date.now(),
+//     volume: req.query.volume,
+//   });
+//   res.send(200);
+//   shower.save(function(err) {
+//     if(err) { 
+//       console.log(err); 
+//     } else {
+//       console.log('saving shower ...');
+//     };
+//   });
+// });
 
 // port
 app.listen(process.env.PORT || 5000);
@@ -177,4 +249,4 @@ function ensureAuthenticated(req, res, next) {
   res.redirect('/');
 }
 
-module.exports = app
+module.exports = app;
